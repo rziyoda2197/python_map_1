3eb35c8aac2626a4fa0c04d5c89e8e31fe4f7366
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-17 16:42:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+sonlar = [-3, -1, 0, 2, 4]
+natija = list(map(lambda x: x**3, sonlar))
+print(natija)
+
+sonlar = list(range(1, 11))
+natija = list(map(lambda x: x**3, sonlar))
+print(natija)
+
+sonlar = [10, 12, 15, 20, 25]
+natija = list(map(lambda x: x**3, sonlar))
+print(natija)
